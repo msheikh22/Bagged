f66ad6696feb90f2f3b96e64513e88b9e9b37e71
msheikh22 <mohamedsheikh22@outlook.com>
2026-01-17 21:16:59 +0000

    Add routes for creating and retrieving reviews

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
+const { createReview, getShopperReviews } = require('../controllers/reviewController');
 
+router.post('/', protect, createReview);
+router.get('/:id', protect, getShopperReviews);
+
+module.exports = router;
