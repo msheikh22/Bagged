c3ddf69a8a1edc15ed7d77f68a4518d6dddddc6d
msheikh22 <mohamedsheikh22@outlook.com>
2026-01-17 21:16:21 +0000

    Add routes for creating and retrieving requests

--- backend/routes/requests.js
+++ backend/routes/requests.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
+const { createRequest, getUserRequests } = require('../controllers/requestController');
 
+router.post('/', protect, createRequest);
+router.get('/:id', protect, getUserRequests);
+
+module.exports = router;
