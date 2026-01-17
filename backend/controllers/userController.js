df3578607422e737e872bbcf9478ba3a59cbb5f5
msheikh22 <mohamedsheikh22@outlook.com>
2026-01-17 21:14:30 +0000

    Add user profile and loyalty retrieval methods

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,1 +1,12 @@
+const User = require('../models/User');
+const Loyalty = require('../models/Loyalty');
 
+exports.getProfile = async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.json(user);
+};
+
+exports.getLoyalty = async (req, res) => {
+  const loyalty = await Loyalty.findOne({ user: req.user._id });
+  res.json({ points: loyalty ? loyalty.points : 0 });
+};
