7b91b41055360133c2f5027d33ed65c1c729ec11
msheikh22 <mohamedsheikh22@outlook.com>
2026-01-17 21:14:51 +0000

    Add request creation and retrieval functions

--- backend/controllers/requestController.js
+++ backend/controllers/requestController.js
@@ -1,1 +1,18 @@
+const Request = require('../models/Request');
 
+exports.createRequest = async (req, res) => {
+  const { title, description, budget, deliveryLocation } = req.body;
+  const request = await Request.create({
+    buyer: req.user._id,
+    title,
+    description,
+    budget,
+    deliveryLocation
+  });
+  res.status(201).json(request);
+};
+
+exports.getUserRequests = async (req, res) => {
+  const requests = await Request.find({ buyer: req.user._id }).populate('quotes');
+  res.json({ requests });
+};
