7c97994aa967535d6645b7db68c561c3cf49ae5b
msheikh22 <mohamedsheikh22@outlook.com>
2026-01-17 21:02:47 +0000

    Add BuyerDashboard component to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import BuyerDashboard from './BuyerDashboard';
 
+const App = () => {
+  const userId = 'user123'; // Replace with login logic later
+  return <BuyerDashboard userId={userId} />;
+};
+
+export default App;
