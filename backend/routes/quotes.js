2ab7c06c4adb7372bf999879d45c63abd6723569
msheikh22 <mohamedsheikh22@outlook.com>
2026-01-17 21:16:39 +0000

    Add payment route for quotes with authentication

--- backend/routes/quotes.js
+++ backend/routes/quotes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
+const { payQuote } = require('../controllers/quoteController');
 
+router.post('/pay/:id', protect, payQuote);
+
+module.exports = router;
