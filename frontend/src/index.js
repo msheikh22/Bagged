667900e2c0cb607daf2584051c28741fd7839236
msheikh22 <mohamedsheikh22@outlook.com>
2026-01-17 21:02:02 +0000

    Set up React app with StripeProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import StripeProvider from './StripeProvider';
+import './styles.css';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <StripeProvider>
+      <App />
+    </StripeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
