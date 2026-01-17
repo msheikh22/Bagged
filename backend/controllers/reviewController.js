eb212df375cae603d82d659fafed8c360672371d
msheikh22 <mohamedsheikh22@outlook.com>
2026-01-17 21:15:10 +0000

    Add create and get functions for reviews

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -1,1 +1,18 @@
+const Review = require('../models/Review');
 
+exports.createReview = async (req, res) => {
+  const { requestId, shopperId, rating, comment } = req.body;
+  const review = await Review.create({
+    request: requestId,
+    shopper: shopperId,
+    buyer: req.user._id,
+    rating,
+    comment
+  });
+  res.status(201).json(review);
+};
+
+exports.getShopperReviews = async (req, res) => {
+  const reviews = await Review.find({ shopper: req.params.id });
+  res.json(reviews);
+};
